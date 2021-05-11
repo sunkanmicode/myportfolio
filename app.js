9e279756bdc3cd313d1c4cfafe6bf717781d1ed7
sunkanmicode <oguntimehins@gmail.com>
2021-05-11 05:39:11 -0700

    adding view engine

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+const pathDir = path.join(__dirname, "./public");
+app.use(express.static(pathDir));
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) =>{
-    res.send('hello world')
+    res.render('index')
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about')
 });
 
 app.listen(4000, ()=>{
